b7e2501ae417e781f7d4d89d386ffb75b05f0b8e
Axel-Hellman <axel.hellman@outlook.com>
2018-03-23 17:41:49 +0100

    Startpunkt till arbetet

--- app/src/main/java/com/example/axel/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/axel/hellosensor/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.axel.hellosensor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Test
+    public void compass(View view){
+    Intent intent = new Intent(this, CompassActivity.class);
+    startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
