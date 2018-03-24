9088aecf7788c96a371a8cce8ac08273ff0bff8f
Axel-Hellman <axel.hellman@outlook.com>
2018-03-24 10:39:47 +0100

    Startpunkt till arbetet v2

--- app/src/main/java/com/example/axel/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/axel/hellosensor/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     startActivity(intent);
     }
 
+    public void Accelerometer(View view){
+        Intent intent = new Intent(this, Accelerometer.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
